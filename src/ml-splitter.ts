822ae4cb79e610ab5e5886c570659dc139a53f6e
Katsusuke Shimizu <katsusuke@gmail.com>
2019-11-29 14:25:31 +0900

    refactoring

--- src/ml-splitter.ts
+++ src/ml-splitter.ts
@@ -5,16 +5,29 @@ import { matches } from './string-utils'
 export type LanguageIndex = number | undefined
 export type LanguagedString = [LanguageIndex, string]
 export type FixedLanguagedString = [number, string]
+export interface LanguageSentence {
+  str: string
+  languageIndex: LanguageIndex
+}
+
+export function splitLanguageSentences(str: string): LanguageSentence[] {
+  if (str.length === 0) {
+    return []
+  } else {
+    const mm = matches(RegExp('([^|]+)(\\|+)?','g'), str)
+    return mm.map(m => ({str: m[1], languageIndex: m[2] !== undefined ? m[2].length : undefined}))
+  }  
+}
 
 export function getMaximumLanguageIndex(str: string): LanguageIndex {
   if (str.length === 0) {
     return undefined
   } else {
-    const mm = matches(RegExp('([^|]+)(\\|+)?','g'), str)
+    const ls = splitLanguageSentences(str).reverse()
     let maximumIndex = 0
-    mm.reverse().forEach(m => {
-      if (m[2] !== undefined) {
-        maximumIndex = Math.max(m[2].length, maximumIndex)
+    ls.forEach(ls => {
+      if (ls.languageIndex !== undefined) {
+        maximumIndex = Math.max(ls.languageIndex, maximumIndex)
       }
     })
     return maximumIndex
