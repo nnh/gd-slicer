91008ef20608a953b819ca10bd0178bf56a7addb
Katsusuke Shimizu <katsusuke@gmail.com>
2020-03-02 20:11:32 +0900

    add test

--- test/string-utils.test.ts
+++ test/string-utils.test.ts
@@ -5,14 +5,16 @@ describe("matches", () => {
   context('mutiple matches', () => {
     it('returns spletted chars', () => expect(matches(/[a-z]/g, 'abc').map(m => m[0])).to.eql(['a', 'b', 'c']))
   })
-  context('with no end marked string', () => {
-    it('returns spletted sentences', () => {
-      expect(matches(/([^|]+$|[^|]*(\|+))/g, '||| ').map(m => m[0])).to.eql(['|||', ' '])
+  context('with language splitting regex', () => {
+    context('with no end marked string', () => {
+      it('returns spletted sentences', () => {
+        expect(matches(/([^|]+$|[^|]*(\|+))/g, '||| ').map(m => m[0])).to.eql(['|||', ' '])
+      })
     })
-  })
-  context('with | ended string', () => {  
-    it('returns spletted sentences', () => {
-      expect(matches(/([^|]+$|[^|]*(\|+))/g, ' |||').map(m => m[0])).to.eql([' |||'])
+    context('with | ended string', () => {  
+      it('returns spletted sentences', () => {
+        expect(matches(/([^|]+$|[^|]*(\|+))/g, ' |||').map(m => m[0])).to.eql([' |||'])
+      })
     })
   })
 })
