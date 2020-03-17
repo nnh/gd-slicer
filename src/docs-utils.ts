ad3e066cb975f7dfacc87bea4174e69f0b62c636
Katsusuke Shimizu <katsusuke@gmail.com>
2020-03-17 11:12:06 +0900

    refactoring naming

--- src/docs-utils.ts
+++ src/docs-utils.ts
@@ -8,12 +8,12 @@ export function copyFile(fromId: string, newFileName: string) {
 
 export type ParentalElement = Document.Body | Document.TableCell | Document.Paragraph | Document.ListItem
 export type EachChildCallback<T> = (element: Document.Element, parent: ParentalElement) => T
-export type EachElementCallback<T> = (child: Document.Element, results: T[]) => T
+export type AfterChildrenCallback<T> = (child: Document.Element, results: T[]) => T
 
 export function reverseChild<T>(element: Document.Element,
                              parent: ParentalElement,
-                             callback: EachChildCallback<T>,
-                             eachElementCallback: EachElementCallback<T>): T {
+                             eachChildCallback: EachChildCallback<T>,
+                             afterChildrenCallback: AfterChildrenCallback<T>): T {
   const elementType = element.getType()
   switch (elementType) {
     case DocumentApp.ElementType.TABLE: {
@@ -22,28 +22,28 @@ export function reverseChild<T>(element: Document.Element,
         const row = element.asTable().getRow(r)
         for (let c = row.getNumCells() - 1; 0 <= c; --c) {
           const cell = row.getCell(c)
-          results.push(reverseChildren(cell, callback, eachElementCallback))
+          results.push(reverseChildren(cell, eachChildCallback, afterChildrenCallback))
         }
       }
-      return eachElementCallback(element, results)
+      return afterChildrenCallback(element, results)
     }
     case DocumentApp.ElementType.PARAGRAPH: {
-      return reverseChildren(element.asParagraph(), callback, eachElementCallback)
+      return reverseChildren(element.asParagraph(), eachChildCallback, afterChildrenCallback)
     }
     case DocumentApp.ElementType.LIST_ITEM: {
-      return reverseChildren(element.asListItem(), callback, eachElementCallback)
+      return reverseChildren(element.asListItem(), eachChildCallback, afterChildrenCallback)
     }
     default: {
-      return callback(element, parent)
+      return eachChildCallback(element, parent)
     }
   }
 }
 
-export function reverseChildren<T>(parent: ParentalElement, callback: EachChildCallback<T>, eachElementCallback: EachElementCallback<T>): T {
+export function reverseChildren<T>(parent: ParentalElement, eachChildCallback: EachChildCallback<T>, afterChildrenCallback: AfterChildrenCallback<T>): T {
   let results = [] as T[]
   for (let i = parent.getNumChildren() - 1; 0 <= i; --i) {
     const child = parent.getChild(i)
-    results.push(reverseChild(child, parent, callback, eachElementCallback))
+    results.push(reverseChild(child, parent, eachChildCallback, afterChildrenCallback))
   }
-  return eachElementCallback(parent, results)
+  return afterChildrenCallback(parent, results)
 }
