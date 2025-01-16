cc742e695da46b9d6fb45075b039bbdf9bb6601f
MarikoOhtsuka <mariko.ohtsuka@nnh.go.jp>
2025-01-16 11:33:22 +0900

    Revert unintended formatting and apply necessary fixes
    - Restored original formatting for sources that were incorrectly modified.
    - Applied required changes to ensure code consistency and maintain functionality.

--- src/split-language.ts
+++ src/split-language.ts
@@ -1,89 +1,79 @@
-import { splitString } from "./split-string";
-import { copyFile } from "./docs-utils";
-import Document = GoogleAppsScript.Document;
+import { splitString } from './split-string'
+import { copyFile } from './docs-utils'
+import Document = GoogleAppsScript.Document
 
-function splitChild(
-  element: Document.Element,
-  open: boolean
-): [boolean, boolean] {
-  const elementType = element.getType();
+function splitChild(element: Document.Element, open: boolean): [boolean, boolean] {
+  const elementType = element.getType()
   switch (elementType) {
     case DocumentApp.ElementType.TEXT: {
-      const [text, isOpen] = splitString(element.asText().getText(), open);
-      if (text === "") {
-        return [isOpen, true];
+      const [text, isOpen] = splitString(element.asText().getText(), open)
+      if (text === '') {
+        return [isOpen, true]
       } else {
-        element.asText().setText(text);
-        return [isOpen, false];
+        element.asText().setText(text)
+        return [isOpen, false]
       }
     }
     case DocumentApp.ElementType.TABLE:
       if (open) {
-        let isOpen = true;
+        let isOpen = true
         for (let r = 0; r < element.asTable().getNumRows(); ++r) {
-          const row = element.asTable().getRow(r);
+          const row = element.asTable().getRow(r)
           for (let c = 0; c < row.getNumCells(); ++c) {
-            const cell = row.getCell(c);
-            isOpen = splitChildren(cell, isOpen);
+            const cell = row.getCell(c)
+            isOpen = splitChildren(cell, isOpen)
           }
         }
-        return [isOpen, false];
+        return [isOpen, false]
       } else {
-        return [false, true];
+        return [false, true]
       }
     case DocumentApp.ElementType.LIST_ITEM: {
-      open = splitChildren(element.asListItem(), open);
-      return [open, false];
+      open = splitChildren(element.asListItem(), open)
+      return [open, false]
     }
     case DocumentApp.ElementType.PARAGRAPH: {
-      open = splitChildren(element.asParagraph(), open);
-      return [open, false];
+      open = splitChildren(element.asParagraph(), open)
+      return [open, false]
     }
     default:
       if (open) {
-        return [true, false];
+        return [true, false]
       } else {
-        return [false, true];
+        return [false, true]
       }
   }
 }
 
-function splitChildren(
-  parent:
-    | Document.Body
-    | Document.TableCell
-    | Document.Paragraph
-    | Document.ListItem,
-  open: boolean
-): boolean {
+function splitChildren(parent: Document.Body | Document.TableCell | Document.Paragraph | Document.ListItem, open: boolean): boolean {
   for (let i = 0; i < parent.getNumChildren(); ++i) {
-    const child = parent.getChild(i);
-    const [isOpen, remove] = splitChild(child, open);
-    open = isOpen;
+    const child = parent.getChild(i)
+    const [isOpen, remove] = splitChild(child, open)
+    open = isOpen
     if (remove) {
-      parent.removeChild(child);
+      parent.removeChild(child)
       i--;
     }
   }
-  return open;
+  return open
 }
 
 export function splitLanguage() {
-  const doc = DocumentApp.getActiveDocument();
-  const documentId = doc.getId();
-  const name = doc.getName();
+  const doc = DocumentApp.getActiveDocument()
+  const documentId = doc.getId()
+  const name = doc.getName()
   {
-    const jpId = copyFile(documentId, name + "jp");
-    const jpDoc = DocumentApp.openById(jpId);
-    let open = true;
-    splitChildren(jpDoc.getBody(), open);
-    jpDoc.saveAndClose();
+    const jpId = copyFile(documentId, name + 'jp')
+    const jpDoc = DocumentApp.openById(jpId)
+    let open = true
+    splitChildren(jpDoc.getBody(), open)
+    jpDoc.saveAndClose()
   }
   {
-    const enId = copyFile(documentId, name + "en");
-    const enDoc = DocumentApp.openById(enId);
-    let open = false;
-    splitChildren(enDoc.getBody(), open);
-    enDoc.saveAndClose();
+    const enId = copyFile(documentId, name + 'en')
+    const enDoc = DocumentApp.openById(enId)
+    let open = false
+    splitChildren(enDoc.getBody(), open)
+    enDoc.saveAndClose()
   }
 }
